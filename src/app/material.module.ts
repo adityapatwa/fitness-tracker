2ca42e891cac8908b855d35d9ef7e59fcec588c9
Aditya Patwa <adityapatwa@live.com>
2018-11-28 10:31:31 -0500

    Added the content for the current training and new training screens.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,29 +1,30 @@
 import { NgModule } from '@angular/core';
 import {
-  MatButtonModule, MatCheckboxModule,
-  MatDatepickerModule,
+  MatButtonModule, MatCardModule, MatCheckboxModule,
+  MatDatepickerModule, MatDialogModule,
   MatFormFieldModule,
   MatIconModule,
   MatInputModule, MatListModule,
-  MatNativeDateModule, MatSidenavModule, MatToolbarModule
+  MatNativeDateModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatTabsModule, MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
   imports: [
-    MatButtonModule, MatCheckboxModule,
-    MatDatepickerModule,
+    MatButtonModule, MatCardModule, MatCheckboxModule,
+    MatDatepickerModule, MatDialogModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule, MatListModule,
-    MatNativeDateModule, MatSidenavModule, MatToolbarModule
+    MatNativeDateModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatTabsModule, MatToolbarModule
+
   ],
   exports: [
-    MatButtonModule, MatCheckboxModule,
-    MatDatepickerModule,
+    MatButtonModule, MatCardModule, MatCheckboxModule,
+    MatDatepickerModule, MatDialogModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule, MatListModule,
-    MatNativeDateModule, MatSidenavModule, MatToolbarModule
+    MatNativeDateModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatTabsModule, MatToolbarModule
   ]
 })
 export class MaterialModule {
