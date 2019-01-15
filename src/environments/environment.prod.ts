9a33b861d85265074f83af965715ad6224316c0b
Aditya Patwa <adityapatwa@live.com>
2019-01-15 18:03:05 -0500

    Added the configuration for the production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCPSVvr8EQuRj4SG9YyA1t1EBM7jdK3UV8',
+    authDomain: 'fitness-tracker-e0ea8.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-e0ea8.firebaseio.com',
+    projectId: 'fitness-tracker-e0ea8',
+    storageBucket: 'fitness-tracker-e0ea8.appspot.com',
+    messagingSenderId: '437828920975'
+  }
 };
